36c00213f63750243cd986fe797727e5731e055d
adamberman <adamberman13@gmail.com>
2015-01-03 17:37:03 -0800

    create users controller

---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,29 @@
+module Api
+  class UsersController < ApplicationController
+    def create
+      @user = User.new(user_params)
+
+      if @user.save
+        log_in!(@user)
+        render :show, status: :created
+      else
+        render json: @user.errors.full_messages, status: :unprocessable_entity
+      end
+    end
+
+    def show
+      return current_user
+      @current_user ||= User.find_by_session_token(session[:session_token])
+
+    private
+
+    def user_params
+      params.require(:user).permit(
+        :first_name,
+        :last_name, 
+        :email,
+        :password
+      )
+    end
+  end
+end
