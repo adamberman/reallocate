a21ab31398eb3af9e6dec5b2947b3beb5f923be8
adamberman <adamberman13@gmail.com>
2015-01-03 17:07:15 -0800

    setup db with pg, create user table, create user model with methods necessary for auth

---
+++ app/models/user.rb
@@ -0,0 +1,40 @@
+class User < ActiveRecord::Base
+	validates :first_name, :last_name, :email, :session_token, presence: true
+  validates :password, length: { minimum: 6, allow_nil: true }
+  validates :email, uniqueness: true
+
+  attr_reader :password
+  after_initialize :ensure_session_token
+
+  def self.find_by_credentials(user_params)
+    user = User.find_by_email(user_params[:email])
+    user.try(:is_password?, user_params[:password]) ? user : nil
+  end
+
+   def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+  
+  def reset_session_token!
+    self.session_token = SecureRandom.base64(16)
+    self.save!
+    self.session_token
+  end
+  
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+  
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    return nil if user.nil?
+    user.is_password?(password) ? user : nil
+  end
+  
+  private
+  
+  def ensure_session_token
+    self.session_token ||= SecureRandom.base64(16)
+  end
+end
