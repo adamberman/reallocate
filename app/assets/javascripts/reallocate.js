0ef26efe320bcb0b90fa9853f278415ddd1bbaee
adamberman <adamberman13@gmail.com>
2015-01-03 18:32:20 -0800

    initialize router

--- app/assets/javascripts/reallocate.js
+++ app/assets/javascripts/reallocate.js
@@ -4,7 +4,8 @@ window.Reallocate = {
   Views: {},
   Routers: {},
   initialize: function() {
-    alert('Hello from Backbone!');
+  	new TVBnB.Routers.Router({$rootEl: $('#content')});
+  	Backbone.history.start();
   }
 };
 
