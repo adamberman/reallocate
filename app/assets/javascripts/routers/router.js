b0b1d23757b1f03f2578da5dfa94bfd749676a65
adamberman <adamberman13@gmail.com>
2015-01-03 18:41:40 -0800

    set up basic router methods for signup/login

--- app/assets/javascripts/routers/router.js
+++ app/assets/javascripts/routers/router.js
@@ -3,4 +3,22 @@ Reallocate.Routers.Router = Backbone.Router.extend({
 	initialize: function (options) {
 		this.$rootEl = options.$rootEl;
 	},
+
+	routes: {
+		"": "main"
+		"login": "login",
+		"signup": "signup"
+	},
+
+	main: function () {
+
+	},
+
+	login: function () {
+
+	},
+
+	signup: function () {
+		
+	}
 })
