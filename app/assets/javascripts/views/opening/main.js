4d1c92459f85423b1a37222a150a013388be99f7
adamberman <adamberman13@gmail.com>
2015-01-03 19:03:47 -0800

    create opening/main view

---
+++ app/assets/javascripts/views/opening/main.js
@@ -0,0 +1,12 @@
+TVBnB.Views.Main = Backbone.View.extend({
+
+	className: 'main',
+
+	template: JST['opening/main'],
+
+	render: function () {
+		var content = this.template();
+		this.$el.html(content);
+		return this;
+	}
+})
