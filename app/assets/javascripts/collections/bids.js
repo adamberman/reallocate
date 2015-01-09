b11753a55e1720738ecab5190e1bab3339c6938e
adamberman <adamberman13@gmail.com>
2015-01-09 12:36:21 -0800

    add comparator

--- app/assets/javascripts/collections/bids.js
+++ app/assets/javascripts/collections/bids.js
@@ -2,5 +2,15 @@ Reallocate.Collections.Bids = Backbone.Searchable.extend({
 
 	model: Reallocate.Models.Bid,
 
-	url: '/api/bids'
+	url: '/api/bids',
+
+	comparator: function (card1, card2) {
+		if (card1.id > card2.id) {
+			return -1;
+		} else if (card1.id < card2.id) {
+			return 1;
+		} else {
+			return 0;
+		}
+	}
 })
