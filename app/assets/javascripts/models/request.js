5b9ec4fc8e9f86886bf4ddc2c5c9a8feae7c41b9
adamberman <adamberman13@gmail.com>
2015-01-08 17:48:52 -0800

    add parse method to request bb model to create bids collection

--- app/assets/javascripts/models/request.js
+++ app/assets/javascripts/models/request.js
@@ -1,5 +1,20 @@
 Reallocate.Models.Request = Backbone.Model.extend({
 
-	urlRoot: "/api/requests"
+	urlRoot: "/api/requests",
+
+	bids: function () {
+		if (!this._bids) {
+			this._bids = new Reallocate.Collections.Bids([], { request: this });
+		}
+		return this._bids;
+	},
+
+	parse: function (response) {
+		if (response.bids) {
+			this.requests().set(response.bids, { parse: true });
+			delete response.bids;
+		}
+		return response;
+	}
 
 })
