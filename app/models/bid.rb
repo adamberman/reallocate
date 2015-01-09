190fa93b521e60dd20b8a2d24c1fcbe5d7261761
adamberman <adamberman13@gmail.com>
2015-01-08 16:15:15 -0800

    debug validations

--- app/models/bid.rb
+++ app/models/bid.rb
@@ -1,6 +1,6 @@
 class Bid < ActiveRecord::Base
 	validates :user, :request, :content, :writer, presence: true
-	validates :writer, inclusion: { in: ['User, Requester'], message: 'Writer must be either "User" or "Requester"' }
+	validates :writer, inclusion: { in: ['User', 'Requester'], message: 'Writer must be either "User" or "Requester"' }
 
 	belongs_to :user
 	belongs_to :request
