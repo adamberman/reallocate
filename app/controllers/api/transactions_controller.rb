c23937f919a6b109d312b93abbfe17e8cf568c7a
adamberman <adamberman13@gmail.com>
2015-01-13 11:15:34 -0800

    add transactions controller

--- app/controllers/api/transactions_controller.rb
+++ app/controllers/api/transactions_controller.rb
@@ -1,13 +1,19 @@
 module Api
 	class TransactionsController < ApplicationController
 		def create
-			
+			@transaction = current_user.responded_transactions.new(transaction_params)
+
+			if @transaction.save
+				render :show, status: :created
+			else
+				render json: @transaction.errors.full_messages, status: :unprocessable_entity
+			end
 		end
 
 		private
 		
-		# def transaction_params
-		# 	params.require(:transaction)
-		# end
+		def transaction_params
+			params.require(:transaction).permit(:listable_id, :listable_type)
+		end
 	end
 end
