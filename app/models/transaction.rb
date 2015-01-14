ea4a562b1177f1c0b0ba965762ca39836551fe1a
adamberman <adamberman13@gmail.com>
2015-01-13 19:01:45 -0800

    create validation to allow a user to only initialize one transaction per request or offer

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,5 +1,6 @@
 class Transaction < ActiveRecord::Base
 	validates :name, :description, :listable, :respondable, presence: true
+	validates :respondable, uniqueness: { scope: :listable, message: 'You have already initialized a transaction on this item' }
 
 	belongs_to :listable, polymorphic: true
 	belongs_to :respondable, polymorphic: true
