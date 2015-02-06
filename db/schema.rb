02fcb0f7d7ec3c552903189ad36e4c7735171c86
adamberman <adamberman13@gmail.com>
2015-02-05 17:07:07 -0800

    add hours to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150203014224) do
+ActiveRecord::Schema.define(version: 20150206010557) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,14 +54,15 @@ ActiveRecord::Schema.define(version: 20150203014224) do
   end
 
   create_table "users", force: true do |t|
-    t.string   "first_name",      null: false
-    t.string   "last_name",       null: false
-    t.string   "email",           null: false
-    t.string   "password_digest", null: false
-    t.string   "session_token",   null: false
+    t.string   "first_name",                  null: false
+    t.string   "last_name",                   null: false
+    t.string   "email",                       null: false
+    t.string   "password_digest",             null: false
+    t.string   "session_token",               null: false
     t.string   "gravatar_url"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "hours",           default: 0, null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", using: :btree
