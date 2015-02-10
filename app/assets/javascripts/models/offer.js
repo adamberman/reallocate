72b246374d630cfa6d2621970613f96450a86355
adamberman <adamberman13@gmail.com>
2015-02-10 11:06:32 -0800

    create offer bb model

---
+++ app/assets/javascripts/models/offer.js
@@ -0,0 +1,31 @@
+Reallocate.Models.Offer = Backbone.Model.extend({
+
+	urlRoot: "/api/offers",
+
+	transaction: function () {
+		if (!this._transaction) {
+			this._transaction = new Reallocate.Models.Transaction({}, { request: this });
+		}
+		return this._transaction;
+	},
+
+	transactions: function() {
+		if (!this._transactions) {
+			this._transactions = new Reallocate.Collections.Transactions([], { request: this });
+		}
+		return this._transactions;
+	},
+
+	parse: function (response) {
+		if (response.transaction) {
+			this.transaction().set(response.transaction, { parse: true });
+			delete response.transaction;
+		}
+		if (response.transactions) {
+			this.transactions().set(response.transactions, { parse: true });
+			delete response.transactions;
+		}
+		return response;
+	}
+
+})
