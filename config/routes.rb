7bf3955c95583cf91f42a148f59d334438ee1b55
adamberman <adamberman13@gmail.com>
2015-02-10 17:34:11 -0800

    debug routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   root to: 'static_page#root'
-  resource :static_page, only: :root
 
   namespace :api, defaults: { format: :json } do 
     resources :users, only: [:create, :show]
