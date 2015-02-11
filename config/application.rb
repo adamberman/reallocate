89fc88500998610d33b96948a0d606e7ab404591
adamberman <adamberman13@gmail.com>
2015-02-10 18:01:18 -0800

    possible debug

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,6 @@ Bundler.require(*Rails.groups)
 
 module Reallocate
   class Application < Rails::Application
-    config.assets.initialize_on_precompile = false
     config.serve_static_assets = true;
 
     # Settings in config/environments/* take precedence over those specified here.
